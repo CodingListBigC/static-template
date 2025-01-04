ceafefc9981a8ea44e023a798d1dfd487f75ba1b
CodingListBigC <126486972+CodingListBigC@users.noreply.github.com>
2025-01-04 10:56:56 -0500

    Create devFile.sh

---
+++ devFile.sh
@@ -0,0 +1,2 @@
+wget -O homeassistant-supervised.deb https://github.com/home-assistant/supervised-installer/releases/latest/download/homeassistant-supervised.deb
+apt install ./homeassistant-supervised.deb
